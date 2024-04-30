d96f9991b56c2444dcf129aa82600a0e0d2c2e29
10724112322 <10724112322@UNS11DT098874.animaedu.intranet>
2024-04-30 19:05:01 -0300

    first commit

---
+++ cadastroAlunos/src/main/java/Principal/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author 10724112322
+ */
+public class Principal {
+    public static void main(String[] args){
+        System.out.println("Está funcionando");
+    }
+}
